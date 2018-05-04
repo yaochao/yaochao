0719469e0e02877035356a106b35989abf86ca23
yaochao <yaochao365@qq.com>
2018-05-04 14:15:08 +0800

    fix

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from setuptools import setup
 setup(
     name="yaochao",
     packages=['yaochao'],
-    version="0.0.1",
+    version="0.0.2",
     author="yaochao",
     author_email="yaochao365@qq.com",
     description="yaochao",
     keywords=['yaochao'],
     license="MIT",
     url="https://github.com/yaochao",
-    download_url="https://github.com/yaochao/yaochao/archive/0.0.1.tar.gz",
+    download_url="https://github.com/yaochao/yaochao/archive/0.0.2.tar.gz",
 )
